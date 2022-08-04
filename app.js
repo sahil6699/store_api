a88919731e580694269bd246e32dc194897f5837
sahil6699 <sahilaneja66199@gmail.com>
2022-08-04 18:59:09 +0530

    boiler plate code for store api

---
+++ app.js
@@ -0,0 +1,14 @@
+require("dotenv").config()
+//aysnc errors
+
+const express = require("express")
+const app = express()
+const port = 3000
+
+app.get("/", (req, res) => {
+  res.send("<h1>hello world</h1>")
+})
+
+app.listen(port, () => {
+  console.log(`Server is listening on port :- ${port}`)
+})

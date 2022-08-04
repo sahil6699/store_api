0b5faeea1d2906bd0f3f383029d8afb4862e80a4
sahil6699 <sahilaneja66199@gmail.com>
2022-08-04 19:18:41 +0530

    basic express app

--- app.js
+++ app.js
@@ -1,14 +1,35 @@
-require("dotenv").config()
-//aysnc errors
+require('dotenv').config()
+//async errors
 
-const express = require("express")
+const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
-app.get("/", (req, res) => {
-  res.send("<h1>hello world</h1>")
+const notFoundMiddleware = require('./middleware/not-found')
+const errorMiddleware = require('./middleware/error-handler')
+
+//middleware
+app.use(express.json())
+
+//routes
+app.get('/', (req, res) => {
+  res.send('<h1>Store API</h1><a href="/api/v1/products">products route</a>')
 })
 
-app.listen(port, () => {
-  console.log(`Server is listening on port :- ${port}`)
-})
+//products routes
+
+app.use(notFoundMiddleware)
+app.use(errorMiddleware)
+
+const start = async () => {
+  //connectDB
+  try {
+    app.listen(port, () => {
+      console.log(`Server is listening on port :- ${port}`)
+    })
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+start()

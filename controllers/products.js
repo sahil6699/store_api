d7950da395d7b2613e0007754f43cef25a6133fc
sahil6699 <sahilaneja66199@gmail.com>
2022-08-16 08:48:42 +0530

    Update products.js

--- controllers/products.js
+++ controllers/products.js
@@ -2,16 +2,22 @@ const Product = require('../models/products')
 
 const getAllProductsStatic = async (req, res) => {
   const products = await Product.find({
-    page: 2,
+    name: 'bar stool',
   })
   res.status(200).json({ products, nbHits: products.length })
 }
 
 const getAllProducts = async (req, res) => {
-  const { feature } = req.query
+  const { feature, company, name } = req.query
   const queryObject = {}
   if (feature) {
     queryObject.feature = feature === 'true' ? true : false
+  }
+  if (company) {
+    queryObject.company = company
+  }
+  if (name) {
+    queryObject.name = name
   }
   console.log(queryObject)
   const products = await Product.find(queryObject)
